0b5f3c0f0e01b6787804027ef7e66f5d1a2f309f
aynurasadovasideas <aynur.asadova.00@gmail.com>
2022-08-29 15:34:14 +0400

    first commit

---
+++ session2-ui-components/Helper/MockData.swift
@@ -0,0 +1,19 @@
+//
+//  MockData.swift
+//  session2-ui-components
+//
+//  Created by Aynur Asadova on 2022-08-29.
+//
+
+import Foundation
+
+class MockData {
+    static let nfts = [
+        NFT(
+            ethPrice: 0.05,
+            dollarPrice: 130,
+            imgName: "prisoner-monkey",
+            description: "The specs:\n Each Bored Ape is unique and programmatically generated from over 170 possible traits, including expression, headwear, clothing, and more. All apes are dope, but some are rarer than others.",
+            name: "Morning Gorgeous")
+    ]
+}
---
+++ session2-ui-components/Model/NFT.swift
@@ -0,0 +1,17 @@
+//
+//  NFT.swift
+//  session2-ui-components
+//
+//  Created by Aynur Asadova on 2022-08-29.
+//
+
+import Foundation
+import UIKit
+
+struct NFT {
+    let ethPrice: Double
+    let dollarPrice: Double
+    let imgName: String
+    let description: String
+    let name: String
+}
